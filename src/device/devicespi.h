3395189d336e3649f7dcfcc66bc446e45079f349
hebaishi <hebaishi.husam@gmail.com>
2013-08-07 02:17:28 +0100

    devicespi.h modified for LDP6803

--- src/device/devicespi.h
+++ src/device/devicespi.h
@@ -1,20 +1,20 @@
 /*
- * boblight
- * Copyright (C) Bob  2012 
- * 
- * boblight is free software: you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * boblight is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License along
- * with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+* boblight
+* Copyright (C) Bob 2012
+*
+* boblight is free software: you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the
+* Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* boblight is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+* See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along
+* with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #ifndef CDEVICESPI
 #define CDEVICESPI
@@ -36,10 +36,12 @@ class CDeviceSPI : public CDevice
     bool WriteBuffer();
 
     CSignalTimer m_timer;
-    int          m_fd;
-    uint8_t*     m_buff;
-    int          m_buffsize;
-    float        m_max;
+    int m_fd;
+  int counter;
+    uint8_t* m_buff;
+    int m_buffsize;
+    float m_max;
+	uint16_t r,g,b,d;
 };
 
 
